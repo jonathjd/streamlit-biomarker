906dc8ff9f3e28c22f812551987869757a24888e
Jon Dickinson <jon.dickinson17@gmail.com>
2022-10-27 11:31:08 -0700

    Add cards and sidebar along with styling

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-st.set_page_config(page_title='Pathway Analysis', layout='centered')
+st.set_page_config(page_title='Pathway Analysis', layout='wide')
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-h = st.container()
-h.title('GSDE Pathway Analysis Tool')
-st.markdown("""---""")
+with st.sidebar:
+    st.title('GSDE Pathway Analysis Tool')
+    target_markers = st.text_input("Paste list of target biomarkers (If more than one separate with a space).")
+    if target_markers:
+        st.success("List uploaded successfully!")
+    else:
+        st.info('Upload a list of biomarkers!', icon="ℹ️")
 
-body = st.container()
-target_markers = body.text_input("Paste list of target biomarkers (If more than one separate with a space).")
-st.markdown("""---""")
 
 #--Logic--
 marker_list = target_markers.split(':')
@@ -28,27 +33,66 @@ targets_df.drop_duplicates(
     inplace=True
     )
 
+#-- DataFrames --
 menu = pd.read_excel('excel_input/soma_menu-st.xlsx')
 overlap = targets_df.merge(menu, on='uniprotid')
 overlap.rename(columns={'uniprotid': 'UniProtID', 'protein_desc': 'Protein Description'}, inplace=True)
-#----
+display_df = targets_df.merge(menu, on='uniprotid', how='right')
+display_df.rename(columns={'uniprotid': 'UniProtID', 'protein_desc': 'Present in Menu (Y/N)'}, inplace=True)
 
-tail = st.container()
-tail.subheader("Results")
+# Change all non-null values to col2 to 'Y'
+display_df['Present in Menu (Y/N)'].loc[~display_df['Present in Menu (Y/N)'].isnull()] = 'Y'
+display_df['Present in Menu (Y/N)'].loc[display_df['Present in Menu (Y/N)'].isnull()] = 'N'
 
-#-- Logic for download button--
-def convert_df(df):
-    return df.to_csv().encode('utf-8')
-out = convert_df(overlap)
-#-----
+#---figs---
+targets_len, menu_len, overlap_len = len(targets_df), len(menu), len(overlap)
+performance = [targets_len, menu_len, overlap_len]
+objects = ['Target Biomarkers', 'SomaLogic Menu', 'Overlap With Menu']
 
-tail.download_button(
-    label="Download as CSV file",
-    data=out,
-    file_name="Biomarker Overlap.csv",
-    mime="text/csv"
-)
-tail.success('Biomarkers successfully downloaded!')
+y_pos = np.arange(len(objects))
+fig = plt.figure(figsize=(6,5))
+plt.bar(
+    y_pos, 
+    performance, 
+    align='center', 
+    alpha=0.6,
+    color='#4067E2'
+    )
+plt.xticks(y_pos, objects)
+plt.ylabel('Number of Proteins')
+
+#-- Display figs --
+if target_markers:
+    st.markdown('## Pathway Metrics')
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Total Biomarkers", targets_len)
+    col2.metric("Overlapping Markers", overlap_len)
+    perc = round((overlap_len / targets_len) * 100)
+    col3.metric("SomaScan Coverage", f"{perc} %")
+    st.dataframe(display_df, use_container_width=True)
+    st.pyplot(fig)
+
+
+
+
+with st.sidebar:
+    #-- download button --
+    def convert_df(df):
+        return df.to_csv().encode('utf-8')
+    out = convert_df(overlap)
+
+    dl_csv = st.download_button(
+        label="Download Overlap as CSV file",
+        data=out,
+        file_name="Biomarker Overlap.csv",
+        mime="text/csv"
+    )
+
+    if len(overlap) > 0 and dl_csv == True:
+        st.success('Biomarkers successfully downloaded!')
+    if len(overlap) == 0 and dl_csv == True:
+        st.error('Whoops! You downloaded an empty list', icon="🚨")
+    
 
 
 
